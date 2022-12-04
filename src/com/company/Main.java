4fda1d0bc6fdd910b12e6cf24276b86126dad231
Admin <dmtr2001@mail.ru>
2022-12-04 14:49:00 +0700

    new branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,24 +12,24 @@ public class Main {
 //        Cat begemot = new Cat("Бегемот",4);
 //        begemot.meow();
 
-//        human maksim = new human("Максим","Минск",1988,"бренд-менеджером");
-//        human ann = new human("Анна","Москва",1993,"методистом образовательных программ");
-//        human Kate = new human("Катя","Калининград",1992,"продакт-менеджером");
-//        human Artem = new human("Артем","Москва",1995,"директором по развитию бизнеса");
-//
-//        maksim.human();
-//        ann.human();
-//        Kate.human();
-//        Artem.human();
+        human maksim = new human("Максим", "Минск", -789, "бренд-менеджером");
+        human ann = new human("Анна", "Москва", 1993, "методистом образовательных программ");
+        human Kate = new human("Катя", "", 1992, "продакт-менеджером");
+        human Artem = new human("Артем", "Москва", 1995, "директором по развитию бизнеса");
+
+        maksim.human();
+        ann.human();
+        Kate.human();
+        Artem.human();
 //
 //        System.out.println(maksim.toString());
 //        System.out.println(ann.toString());
 //        System.out.println(Kate.toString());
 //        System.out.println(Artem.toString());
-
-
-
-        Care lada_Granta = new Care("Lada","Granta",1.7,"Желтого цвета",2015,"Россия");
-        System.out.println(lada_Granta.toString());
+//
+//
+//
+//        Care lada_Granta = new Care("Lada","Granta",1.7,"Желтого цвета",2015,"Россия");
+//        System.out.println(lada_Granta.toString());
     }
 }
